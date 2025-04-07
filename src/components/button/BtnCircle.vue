3f5ab2f279d3200cd3dd30d1e8b31a1e181cc57b
winA <wina418@kw.ac.kr>
2025-04-07 16:32:58 +0900

    [feat | #7] 플로팅버튼구현

---
+++ src/components/button/BtnCircle.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="btn-circle" :style="{ backgroundColor: color }" @click="$emit('click')">
+    <img :src="icon" alt="icon" class="icon" />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    default: 'var(--color-primary)',
+  },
+  icon: {
+    type: String,
+  },
+})
+// 클릭이벤트를 받아서 부모에게 전달
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.btn-circle {
+  border-radius: 100%;
+  width: 3.75rem;
+  height: 3.75rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  color: var(--color-white);
+}
+
+.icon {
+  width: 2.5rem;
+  height: 2.5rem;
+  color: var(--color-white);
+}
+</style>

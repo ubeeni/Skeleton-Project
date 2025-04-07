81029e1ca040481150b81479b02e5b5e87e15fcd
winA <wina418@kw.ac.kr>
2025-04-07 17:52:26 +0900

    [ feat | #13] 버튼컴포넌트생성

--- src/components/button/BtnLg.vue
+++ src/components/button/BtnLg.vue
@@ -1,4 +1,39 @@
-<template>btn-lg</template>
+<template>
+  <button
+    class="btn-lg-wrapper bodySemibold18px"
+    :style="{ backgroundColor: color }"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
 
-<script setup></script>
-<style scoped></style>
+<script setup>
+defineProps({
+  color: {
+    type: String,
+    default: 'var(--color-primary)',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+})
+
+defineEmits(['click'])
+</script>
+
+<style scoped>
+.btn-lg-wrapper {
+  border-radius: 1rem;
+  width: 25rem;
+  height: auto;
+  color: var(--color-white);
+  text-align: center;
+  padding: 1.25rem 0;
+}
+
+.btn-lg-wrapper:hover {
+  background: var(--color-hover);
+}
+</style>
--- src/pages/notFoundPage/NotFoundPage.vue
+++ src/pages/notFoundPage/NotFoundPage.vue
@@ -1,4 +1,15 @@
-<template>notfound</template>
+<template>
+  <BtnLg text="추가" @click="handleClick" color="var(--color-secondary)" />
+  <BtnMed text="추가" @click="handleClick" color="var(--color-primary)" />
+</template>
 
-<script setup></script>
+<script setup>
+import BtnLg from '@/components/button/BtnLg.vue'
+import BtnMed from '@/components/button/BtnMed.vue'
+
+function handleClick() {
+  alert('Button clicked!')
+}
+</script>
+
 <style scoped></style>

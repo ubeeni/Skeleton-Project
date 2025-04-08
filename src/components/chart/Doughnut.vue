25d6caa662619bd0d325734eb4d8f2c5bf6d9cad
winA <wina418@kw.ac.kr>
2025-04-08 13:06:51 +0900

    [feat | #18] 레이아웃설정

--- src/components/chart/Doughnut.vue
+++ src/components/chart/Doughnut.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="doughnut-wrapper">
-    <apexchart width="380" type="donut" :options="options" :series="series" />
+    <apexchart class="doughnut-chart" type="donut" :options="options" :series="series" />
   </div>
 </template>
 
@@ -13,16 +13,9 @@ const series = ref([44, 55, 41, 17, 15])
 // 옵션 설정
 const options = ref({
   chart: {
-    type: 'donut',
+    type: 'doughnut',
   },
   labels: ['카페', '교통', '마트', '쇼핑', '기타'],
-  colors: [
-    'var(--color-primary)',
-    'var(--color-secondary)',
-    'var(--color-accent)',
-    'var(--color-expense)',
-    'var(--color-income)',
-  ],
   legend: {
     position: 'bottom',
   },
@@ -36,6 +29,14 @@ const options = ref({
 .doughnut-wrapper {
   width: 100%;
   height: 100%;
-  background-color: var(--color-light);
+  background-color: var(--color-white);
+  border-radius: 2rem;
+  box-shadow: 0 0 1rem var(--color-light3);
+  padding: 0.5rem;
+}
+
+.doughnut-chart {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/chart/Line.vue
+++ src/components/chart/Line.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="line-wrapper">
-    <apexchart width="500" type="line" :options="options" :series="series"></apexchart>
+    <apexchart class="line-chart" type="line" :options="options" :series="series"></apexchart>
   </div>
 </template>
 
@@ -10,7 +10,8 @@ import { ref } from 'vue'
 // 옵션 설정
 const options = ref({
   chart: {
-    id: 'vuechart-example',
+    id: 'line-chart',
+    type: 'line',
   },
   xaxis: {
     categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
@@ -20,8 +21,14 @@ const options = ref({
 // 데이터 시리즈
 const series = ref([
   {
-    name: 'series-1',
-    data: [30, 40, 45, 50, 49, 60, 70, 91],
+    name: 'income',
+    data: [30, 40, 45, 50, 49, 60, 70, 70],
+    color: 'var(--color-income)',
+  },
+  {
+    name: 'expense',
+    data: [20, 30, 35, 40, 39, 50, 60, 81],
+    color: 'var(--color-expense)',
   },
 ])
 </script>
@@ -30,6 +37,14 @@ const series = ref([
 .line-wrapper {
   width: 100%;
   height: 100%;
-  background-color: var(--color-light);
+  background-color: var(--color-white);
+  border-radius: 2rem;
+  box-shadow: 0 0 1rem var(--color-light3);
+  padding: 0.5rem;
+}
+
+.line-chart {
+  width: 100%;
+  height: 100%;
 }
 </style>
